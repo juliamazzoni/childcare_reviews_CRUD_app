f6ae4b72d0e83afefa5e0c6651459816aebefc67
Julia Mazzoni <juliarmazzoni@gmail.com>
2024-01-23 11:17:35 +1100

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+
+const express = require('express')
+const bcrypt = require('bcrypt')
+const app = express()
+// const expressLayouts = require('express-ejs-layouts')
+const port = 8080
